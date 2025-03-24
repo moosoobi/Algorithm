b88791e1eeb57c009510b62a82fbca000ae1fcc5
moosoobi <97660066+moosoobi@users.noreply.github.com>
2025-03-25 00:26:24 +0900

    [Silver II] Title: 랜선 자르기, Time: 156 ms, Memory: 17212 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int K = Integer.parseInt(input[0]);
+        int N = Integer.parseInt(input[1]);
+
+        int[] lan = new int[K];
+        int max = 0;
+
+        for(int i = 0; i < K; i++) {
+            lan[i] = Integer.parseInt(br.readLine());
+            if(max < lan[i]) max = lan[i];
+        }
+
+        long left = 1;
+        long right = max;
+        long result = 0;
+
+        while(left <= right) {
+            long mid = (left + right) / 2;
+            long count = 0;
+
+            for(int i = 0; i < K; i++) {
+                count += lan[i] / mid;
+            }
+
+            if(count >= N) {
+                result = mid;     // 가능한 답
+                left = mid + 1;   // 더 큰 값도 될 수 있으니 오른쪽 탐색
+            } else {
+                right = mid - 1;  // 너무 많이 잘라서 개수가 부족 → 왼쪽으로
+            }
+        }
+
+        System.out.println(result);
+    }
+}
